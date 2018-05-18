c391750af20977ba4fc16777ce7522c192de8787
bameng <raheemmi@aerohub.co>
2018-05-18 18:25:20 +0800

    add show update delete

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -30,4 +30,32 @@ router.get('/', (req, res) => {
     })
 })
 
+// GETS A SINGLE USER FROM THE DATABASE
+router.get('/:id', (req, res) => {
+    User.findById(req.params.id, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.")
+        if (!user) return res.status(404).send("No user found.")
+        res.status(200).send(user)
+    })
+})
+
+// DELETES A USER FROM THE DATABASE
+router.delete('/:id', (req, res) => {
+    User.findByIdAndRemove(req.params.id, (err, user) => {
+        if (err) return res.status(500).send("There was a problem deleting the user.")
+        res.status(200).send("User " + user.name + " was deleted.")
+    })
+})
+
+// UPDATES A SINGLE USER IN THE DATABASE
+router.put('/:id', (req, res) => {
+
+    User.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    }, (err, user) => {
+        if (err) return res.status(500).send("There was a problem updating the user.")
+        res.status(200).send(user)
+    })
+})
+
 module.exports = router
